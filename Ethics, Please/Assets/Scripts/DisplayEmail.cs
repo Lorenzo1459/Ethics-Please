3e251cddef4ed54720ab442f9285730b25fee829
Lorenzo1459 <lorenzozimerer05@gmail.com>
2025-02-02 18:00:43 -0300

    Emails appearing sequentially

--- Ethics, Please/Assets/Scripts/DisplayEmail.cs
+++ Ethics, Please/Assets/Scripts/DisplayEmail.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DisplayEmail : MonoBehaviour {
-    public ProposalData proposalData;
+    public ProposalDatabase proposalDatabase;
+    private List<int> indexList = new List<int>();
+    private int currentEmailIndex = -1;
 
     public TMP_Text companyNameText;
     public TMP_Text projectTitleText;
@@ -21,15 +23,27 @@ public class DisplayEmail : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        companyNameText.text = proposalData.companyName;
-        projectTitleText.text = proposalData.projectTitle;
-        projectDescriptionText.text = proposalData.projectDescription.Replace("\\n", "\n");
-        projectDescriptionText.text = proposalData.projectDescription;
+        for (int i = 0; i < proposalDatabase.proposals.Count; i++) {
+            indexList.Add(i);
+        }
+        ShuffleEmailIndices();
 
+        NextEmail();
+    }
+
+    void DisplayEmails(int index) {
+        if (index >= 0 && index < proposalDatabase.proposals.Count) {
+            ProposalData proposal = proposalDatabase.proposals[index];
+            companyNameText.text = proposal.companyName;
+            projectTitleText.text = proposal.projectTitle;
+            projectDescriptionText.text = proposal.projectDescription.Replace("\\n", "\n");
+            projectDescriptionText.text = proposal.projectDescription;
+        }
     }
 
 
     public void AcceptEmail() {
+        ProposalData proposalData = proposalDatabase.proposals[currentEmailIndex];
         if (proposalData.hasEthicalIssue) {
             Debug.Log("Você aceitou um e-mail problemático! Isso terá consequências...");
             bool isCorrect = !proposalData.hasEthicalIssue;
@@ -42,10 +56,11 @@ public class DisplayEmail : MonoBehaviour {
             // Adicione recompensas aqui (exemplo: ganhar pontos de integridade)
         }
 
-        //NextEmail();
+        NextEmail();
     }
 
     public void RejectEmail() {
+        ProposalData proposalData = proposalDatabase.proposals[currentEmailIndex];
         if (proposalData.hasEthicalIssue) {
             Debug.Log("Você corretamente rejeitou um e-mail problemático!");
             bool isCorrect = proposalData.hasEthicalIssue;
@@ -58,13 +73,55 @@ public class DisplayEmail : MonoBehaviour {
             // Penalização por rejeitar propostas boas
         }
 
-        //NextEmail();
+        NextEmail();
     }
 
+    void ShuffleEmailIndices() {
+        for (int i = 0; i < indexList.Count; i++) {
+            int tmp = indexList[i];
+            int r = Random.Range(i, indexList.Count);
+            indexList[i] = indexList[r];
+            indexList[r] = tmp;
+        }
+    }
+
+    public void NextEmail() {
+        StartCoroutine(WaitForNextEmail(1.5f));
+    }
+
+    IEnumerator WaitForNextEmail(float time) {
+
+        yield return new WaitForSeconds(time); // Espera dois segundos
+
+        if (indexList.Count > 0) {
+            // Select a random index from the available proposals
+            int randomIndex = Random.Range(0, indexList.Count);
+            currentEmailIndex = indexList[randomIndex];
+
+            // Display the selected proposal
+            DisplayEmails(currentEmailIndex);
+
+            // Remove the selected index to avoid repetition
+            indexList.RemoveAt(randomIndex);
+        } else {
+            // If all proposals have been shown, restart the process
+            Debug.Log("All proposals have been displayed!");
+            indexList = new List<int>();
+            for (int i = 0; i < proposalDatabase.proposals.Count; i++) {
+                indexList.Add(i);
+            }
+
+            // Shuffle the list of proposals again
+            ShuffleEmailIndices();
+
+            // Show the next proposal
+            NextEmail();
+        }
+    }
 
     private IEnumerator FlashColor(bool isCorrect) {
         background.color = isCorrect ? Color.green : Color.red;
-        yield return new WaitForSeconds(0.5f); // Espera meio segundo
+        yield return new WaitForSeconds(1.5f); // Espera 1.5s
         background.color = defaultColor; // Retorna à cor original
     }
 }
