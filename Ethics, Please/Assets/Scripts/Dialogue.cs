b57279dba2de6c1be538d572563ef69daa978a9a
Lorenzo1459 <lorenzozimerer05@gmail.com>
2025-03-08 22:19:51 -0300

    Sistema de diálogos e popUps (em conjunto) funcional

--- Ethics, Please/Assets/Scripts/Dialogue.cs
+++ Ethics, Please/Assets/Scripts/Dialogue.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Dialogue : MonoBehaviour
-{
-    public TextMeshProUGUI textComponent;
+[System.Serializable]
+public class DialogueLine {
     [TextArea(10, 40)]
-    public string[] lines;
+    public string line; // A linha de diálogo
+    public GameObject popUp; // O popUp associado (pode ser null)
+}
+
+public class Dialogue : MonoBehaviour {
+    public TextMeshProUGUI textComponent;
+    public List<DialogueLine> dialogueLines; // Lista de linhas de diálogo com popUps associados
     public float textSpeed;
 
     private int index;
@@ -18,38 +22,55 @@ public class Dialogue : MonoBehaviour
         StartDialogue();
     }
 
-    void Update()
-    {
+    void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            if (textComponent.text == lines[index]) {
+            if (textComponent.text == dialogueLines[index].line) {
                 NextLine();
             } else {
                 StopAllCoroutines();
-                textComponent.text = lines[index];
+                textComponent.text = dialogueLines[index].line;
             }
         }
     }
 
-    void StartDialogue(){
+    void StartDialogue() {
         index = 0;
         StartCoroutine(TypeLine());
     }
 
     IEnumerator TypeLine() {
-        foreach (char c in lines[index].ToCharArray()){
+        // Desativa todos os popUps antes de começar a digitar a nova linha
+        DeactivateAllPopUps();
+
+        // Ativa o popUp da linha atual (se existir)
+        if (dialogueLines[index].popUp != null) {
+            dialogueLines[index].popUp.SetActive(true);
+        }
+
+        // Digita a linha de diálogo caractere por caractere
+        foreach (char c in dialogueLines[index].line.ToCharArray()) {
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
         }
     }
 
     void NextLine() {
-       if (index < lines.Length - 1) {
+        if (index < dialogueLines.Count - 1) {
             index++;
             textComponent.text = string.Empty;
             StartCoroutine(TypeLine());
         } else {
+            // Desativa todos os popUps ao finalizar o diálogo
+            DeactivateAllPopUps();
             gameObject.SetActive(false);
         }
     }
 
+    void DeactivateAllPopUps() {
+        foreach (var dialogueLine in dialogueLines) {
+            if (dialogueLine.popUp != null) {
+                dialogueLine.popUp.SetActive(false);
+            }
+        }
+    }
 }
